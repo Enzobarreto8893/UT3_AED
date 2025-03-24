37bdd7498175186d063ea7be798f2da96e986e0b
Manuela Martony <manuela.martony@correo.ucu.edu.uy>
2025-03-24 19:18:24 -0300

    Ejercicio 3 del TA2

--- UT1-EJ2/src/main/java/org/example/Main.java
+++ UT1-EJ2/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         ContadorPalabras contador = new ContadorPalabras();
-        String frase = "Hola mundo maravilloso y espectacular";
+        String frase = "Si piensas que tu profesor es exigente,... espera conocer a tu jefe!";
         int totalPalabras = contador.contarPalabras(frase);
         System.out.println("Cantidad de palabras: " + totalPalabras);
         int palabrasLargas = contador.ContadorLargoPalabras(frase, 5);
